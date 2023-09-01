b1a8713e534219abdc8d8b0a073cc8197712c7ac
svkxx <143774423+svkxx@users.noreply.github.com>
2023-09-01 19:29:07 +0100

    HWID Table.lua

---
+++ HWID Table.lua
@@ -0,0 +1,21 @@
+--[[
+	Welcome to this Simple ROBLOX Whitelisting for your script(s).
+
+		Author: nNullByte :)
+
+	Note: This was asked of me by a friend that is new to scripting and I thought it might also help you to get an understanding of how simple whitelist systems are made.
+]]
+
+--[[
+	This is the table we are going to use to list all of our HWID / ClientID's.
+
+	All you need to do is to replace the "Insert your HWID here." text with your HWID / Client ID and upload it to pastebin / github.
+
+	Please note that you will have to manually update this table when you want to add another HWID in here.
+]]
+
+return {
+	"Insert your HWID here.",
+	"Insert your HWID here.",
+	"Insert your HWID here.",
+}
